98fd8adf09750060f7b6eaa65b86ef6920301935
vitaliti2706-ctrl <vitaliti2706@gmail.com>
2026-08-09 01:11:48 +0300

    Update material-price-list.js

--- api/material-price-list.js
+++ api/material-price-list.js
@@ -114,7 +114,7 @@ function normalizeRows(rows) {
   };
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'GET') {
     res.setHeader('Allow', 'GET');
     return res.status(405).json({
